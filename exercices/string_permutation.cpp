23ecb5935e049173fefad2dd86c7a06b73fa7bbe
Alonso Lazcano Miguel <AlonsoLazcanoMiguel@JohnDeere.com>
2018-10-24 13:43:06 -0500

    Added string coding exercises

---
+++ exercices/string_permutation.cpp
@@ -0,0 +1,30 @@
+/*
+ * Given two strings, implement a function to decide if one string
+ * is a permutation of the other.
+ *
+ */
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+
+void string_permutation(char* str, char* str2)
+{
+
+}
+
+int main ()
+{
+    return 0;
+}
+
+
+
+/*
+ * TEST_F(permutation_string)
+ * 0. base case 
+ *    
+ * 1. case 
+ *      
+ *     
+ *
+ */
---
+++ exercices/string_reverse.cpp
@@ -0,0 +1,33 @@
+/*
+ * Implement a function which reverses a null termianted
+ * string.
+ *
+ */
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+
+void reverse_string(char* str)
+{
+
+}
+
+int main ()
+{
+    return 0;
+}
+
+
+
+/*
+ * TEST_F(reverse_strings)
+ * 0. base case 
+ *    if ( string == null ) return 
+ * 1. case 
+ *    while (string[i] != '\0' )
+ *        stack.push_back(string[i])
+ *    string[i] = statck.pop()
+ *      
+ *     
+ *
+ */
