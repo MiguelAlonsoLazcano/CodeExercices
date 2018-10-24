06a0c24296b22d5bd5d669dca15d0709e8f305fd
Alonso Lazcano Miguel <AlonsoLazcanoMiguel@JohnDeere.com>
2018-10-24 13:21:21 -0500

    Initial commit, added basic setup

---
+++ exercices/string_all_unique_char.cpp
@@ -0,0 +1,35 @@
+/*
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures
+ *
+ */
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+
+bool all_unique_char (std::string* str)
+{
+
+
+
+}
+int main ()
+{
+    return 0;
+}
+
+
+
+/*
+ * TEST_F(compare_strings)
+ * 0. base case 
+ *    string1.size() != string2.size() return false
+ * 1. case 
+ *    while (string[s] != '\0'
+ *        if ( string[s] == string[s+1] ) 
+ *           flag = true; s++; 
+ *        else
+ *           return flag = false;
+ *    return flag; 
+ *
+ */
